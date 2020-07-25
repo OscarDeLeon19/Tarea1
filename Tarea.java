648d0f5fed26cd3c8cdc9a66b3ab38a3d985b796
OscarDeLeon19 <oscar99deleon19@gmail.com>
2020-07-25 13:02:26 -0600

    Agregamos el codigo para ingresar los strings

--- Tarea.java
+++ Tarea.java
@@ -1,6 +1,12 @@
+import java.util.Scanner;
+
 public class Tarea {
 
     public static void main(String[] args) {
-
+    	Scanner entrada = new Scanner(System.in);
+        System.out.println("Ingresa el primer codigo: ");
+        String Codigo1 = entrada.nextLine();
+        System.out.println("Ingresa el segundo codigo: ");
+        String Codigo2 = entrada.nextLine();
 	}
 }
